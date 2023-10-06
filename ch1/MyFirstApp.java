485fff68b68c0a760242a4d4e23001fd2c785363
Casperliuliuliu <casperliu1118@gmail.com>
2023-10-06 16:11:21 +0800

    first commit with ch1

---
+++ ch1/MyFirstApp.java
@@ -0,0 +1,25 @@
+public class MyFirstApp {
+    // public static void main(String[] args){
+    //     System.out.println("good");
+    //     for(int ii=0; ii<10; ii++){
+    //         System.out.print(ii);
+    //         // System.out.println(Math.random());
+    //         if (ii == 5)
+    //             break;
+    //     }
+    //     int ii = 5;
+    //     while(ii > 0){
+    //         System.out.println(ii);
+    //         // ii++;
+    //         ii = ii -1;
+    //     }
+    // }
+    public static void main(String[] args){
+        String name = "cool";
+        int x = 1;
+        if((name == "cool") & (x > 7))
+            System.out.print("correct");
+        else
+            System.out.print("wrong");
+    }
+}
